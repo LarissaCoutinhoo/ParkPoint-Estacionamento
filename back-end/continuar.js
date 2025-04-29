e97bf5845003a420d62ecbffa1293067a11ef4f5
luis-pedro-dutra-carrocini <luispedrodutracarrocini58@gmail.com>
2025-04-29 05:27:49 -0300

    Adição de Arquivo .env.exemple para conexão com BD

--- back-end/continuar.js
+++
@@ -1,6 +0,0 @@
-/*
-    Alterar dados de entrada
-    Realizar conversa (back-front) saida de veiculos;
-    Realizar conversa (back-front) controle clientes, funcionarios e veiculos;
-    Realizar conversa (back-front) status garagem;
-*/
